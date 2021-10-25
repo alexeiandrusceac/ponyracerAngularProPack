5e44e3554152c228ff0e1efed6c591d186e6ec9a
tekoway <alexeiandrusceac@hotmail.com>
2021-10-25 16:27:08 +0300

    Pony Racer Test Angular Pro Pack - Luni 25.10.2021

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RacesComponent } from './races/races.component';
+import { RaceComponent } from './race/race.component';
+import { PonyComponent } from './pony/pony.component';
+
+@NgModule({
+  declarations: [AppComponent, MenuComponent, RacesComponent, RaceComponent, PonyComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/models/race.model.ts
@@ -0,0 +1,7 @@
+import {PonyModel} from './pony.model';
+export interface RaceModel {
+   name: string;
+   id: number;
+   startInstant: string;
+   ponies: Array<PonyModel>;
+}
