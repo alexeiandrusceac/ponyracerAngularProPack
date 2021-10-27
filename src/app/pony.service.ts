59208e2beb2a02681628d2f5b863c978416fc78b
tekoway <alexeiandrusceac@hotmail.com>
2021-10-27 15:35:26 +0300

    Pony Racer Test Angular Pro Pack - Luni 25.10.2021

--- src/app/pony.service.ts
+++ src/app/pony.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {PonyModel} from './models/pony.model';
+import {filter, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ export class PonyService {
 
   constructor(private httpClient: HttpClient) { }
   get(id: string| null| undefined): Observable<PonyModel> {
-    return this.httpClient.get<PonyModel>('https://ponyracer.ninja-squad.com/api/ponies?id=id');
+    return this.httpClient.get<PonyModel>('https://ponyracer.ninja-squad.com/api/ponies').pipe(
+      map((data: any) => id ? data.filter((item: any) => item.id === Number(id))[0] : data),
+    );
   }
 }
--- src/app/pony/pony.component.ts
+++ src/app/pony/pony.component.ts
@@ -1,36 +1,43 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { PonyModel } from '../models/pony.model';
-import {PonyService} from '../pony.service';
-import {ActivatedRoute} from '@angular/router';
+import { PonyService } from '../pony.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'pr-pony',
-  templateUrl: './pony.component.html',
-  styleUrls: ['./pony.component.css']
+    selector: 'pr-pony',
+    template: `
+        <figure (click)="clicked()" *ngIf="ponyModel">
+            <img [src]="'assets/images/pony-' + this.ponyModel.color.toLowerCase() + '.gif'" [alt]="ponyModel.name"/>
+            <figcaption>{{ ponyModel.name }}</figcaption>
+        </figure>`,
+    /*templateUrl: './pony.component.html',
+    styleUrls: ['./pony.component.css']*/
 })
-export class PonyComponent  implements OnInit{
-  pony: any;
-  constructor(private ponyService: PonyService, private route: ActivatedRoute){
+export class PonyComponent implements OnInit {
+    constructor(
+        private ponyService: PonyService,
+        private route: ActivatedRoute,
+    ) {
 
-  }
+    }
 
-  @Input() ponyModel: PonyModel = {
-    id: 0,
-    name: '',
-    color: ''
-  };
+    @Input() ponyModel!: PonyModel;
 
-  @Output() readonly ponyClicked = new EventEmitter<PonyModel>();
+    @Output() readonly ponyClicked = new EventEmitter<PonyModel>();
 
-  ngOnInit(): void{
-    const id = this.route.snapshot.paramMap.get('ponyID');
+    ngOnInit(): void {
+        const id = this.route.snapshot.paramMap.get('ponyID');
 
-    this.ponyService.get(id).subscribe(ponyModel => this.ponyModel = ponyModel);
-  }
-  getPonyImageUrl(): string {
-    return `assets/images/pony-${this.ponyModel.color.toLowerCase()}.gif`;
-  }
-  clicked(): void {
-    this.ponyClicked.emit(this.ponyModel);
-  }
+        if (!this.ponyModel) {
+            this.ponyService.get(id).subscribe(ponyModel => this.ponyModel = ponyModel);
+        }
+    }
+
+    getPonyImageUrl(): string {
+        return `assets/images/pony-${this.ponyModel.color.toLowerCase()}.gif`;
+    }
+
+    clicked(): void {
+        this.ponyClicked.emit(this.ponyModel);
+    }
 }
